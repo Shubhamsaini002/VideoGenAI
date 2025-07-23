a0b5eeaa9490e6a621d155ca79cda487d85834ed
Shubham saini <148333391+Shubhamsaini002@users.noreply.github.com>
2025-07-23 11:25:21 +0530

    text to mp3

--- app/routers/transcriber.py
+++ app/routers/transcriber.py
@@ -1,15 +1,15 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Form
 from app.services.whisper_service import transcribe_audio
 from app.utils.file_utils import save_upload_file
 from app.utils.tts_generator import generate_test_mp3
 router = APIRouter()
 
-@router.post("/transcribe-audio")
+@router.post("/generate-mp3-to-text")
 async def transcribe(file: UploadFile = File(...)):
     file_path = await save_upload_file(file)
     transcript = transcribe_audio(file_path)
     return {"transcription": transcript}
-@router.post("/generate-test-mp3")
+@router.post("/generate-text-to-mp3")
 async def generate_mp3(text: str = Form(...)):
     file_path = generate_test_mp3(text)
     return {"file_path": file_path}
